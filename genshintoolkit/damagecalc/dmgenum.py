49c02aa36c6908da5eea2472a42c16af3672be84
CorinthusYu <736674365@qq.com>
2022-11-12 00:12:01 +0800

    add: enum default nodes

--- genshintoolkit/damagecalc/dmgenum.py
+++ genshintoolkit/damagecalc/dmgenum.py
@@ -1,7 +1,33 @@
 from enum import Enum
 
 class EnumDefaultNode(Enum):
-    basedmg
-    atk_percent
-    atk
-    baseatk
+    basedmg =  'basedmg'
+
+    atk_percent = 'atk_percent'
+    atk_fixed = 'atk_fixed'
+    baseatk = 'baseatk'
+    baseatk_char = 'baseatk_char'
+    baseatk_wep = 'baseatk_wep'
+    atk = 'atk'
+    atk_extra = 'atk_extra'
+
+    dfn = 'dfn'
+    dfn_extra = 'dfn_extra'
+    dfn_percent = 'dfn_percent'
+    dfn_fixed = 'dfn_fixed'
+    basedfn = 'basedfn'
+
+    hp = 'hp'
+    hp_extra = 'hp_extra'
+    hp_percent = 'hp_percent'
+    hp_fixed = 'hp_fixed'
+    basehp = 'basehp'
+
+    em = 'em'
+    em_extra = 'em_extra'
+
+    # reaction bonus
+    
+
+    
+
